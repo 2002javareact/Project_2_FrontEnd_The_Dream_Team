5fde8f2125a0198489ad621105036838e1f2a105
Nikoloi <ellisnikoloi@yahoo.com>
2020-03-27 03:53:26 -0400

    Added Css

--- project_2_front_end_react/src/components/login-component/LoginComponent.tsx
+++ project_2_front_end_react/src/components/login-component/LoginComponent.tsx
@@ -1,8 +1,8 @@
 import React, { SyntheticEvent } from 'react'
-import { Container, Form, Button, Col, Row, Alert } from 'react-bootstrap'
+//import { Container, Form, Button, Col, Row, Alert } from 'react-bootstrap'
 import { Redirect } from 'react-router'
 import {User} from '../../models/User'
-
+import '../../assets/stylesheet.css'
 interface ILoginProps {
   errorMessage:string
   profile:User
@@ -44,30 +44,44 @@ export class LoginComponent extends React.Component<ILoginProps,ILoginState> {
     return(
       this.props.profile && this.props.profile.id!==0?(
         <Redirect to="/mainpage" />
-      ):(
-        <Container className="p-5">
+       ):(
+      //   <Container className="p-5">
 
-          {this.props.errorMessage && <Alert variant='danger'>{this.props.errorMessage}</Alert>}
-          <Row>
-            <Col>
-              <Form onSubmit={this.submitLogin}>
-                <Form.Group controlId="formUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control onChange={this.handlerUsername} type="text" value={this.state.username} placeholder="Enter Username"/>
-                  <Form.Text className="text-muted">
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control onChange={this.handlerPassword} type="password" value={this.state.password} placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
+      //     {this.props.errorMessage && <Alert variant='danger'>{this.props.errorMessage}</Alert>}
+      //     <Row>
+      //       <Col>
+      //         <Form onSubmit={this.submitLogin}>
+      //           <Form.Group controlId="formUsername">
+      //             <Form.Label>Username</Form.Label>
+      //             <Form.Control onChange={this.handlerUsername} type="text" value={this.state.username} placeholder="Enter Username"/>
+      //             <Form.Text className="text-muted">
+      //             </Form.Text>
+      //           </Form.Group>
+      //           <Form.Group controlId="formPassword">
+      //             <Form.Label>Password</Form.Label>
+      //             <Form.Control onChange={this.handlerPassword} type="password" value={this.state.password} placeholder="Password" />
+      //           </Form.Group>
+      //           <Button variant="primary" type="submit">
+      //             Submit
+      //           </Button>
+      //         </Form>
+      //       </Col>
+      //     </Row>
+
+
+          <form className="login" onSubmit={this.submitLogin}>
+          <input onChange={this.handlerUsername} type="text" placeholder="Username"/>
+          <input type="password" onChange={this.handlerPassword} placeholder="Password" />
+          <p>Forgot your password?</p>
+          <button type="submit">
+            Login 
+          </button>
+        </form>
+      
+      
+
+
+        // </Container>
       )
     )
   }
