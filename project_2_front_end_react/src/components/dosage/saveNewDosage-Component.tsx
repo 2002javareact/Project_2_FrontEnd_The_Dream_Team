fe73f4be386d77e6115162909463a8e0804bce68
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-03-28 02:00:39 -0400

    ReactbootStrap

--- project_2_front_end_react/src/components/dosage/saveNewDosage-Component.tsx
+++ project_2_front_end_react/src/components/dosage/saveNewDosage-Component.tsx
@@ -1,8 +1,9 @@
 import React, { SyntheticEvent } from 'react'
-import { Form, FormGroup, Label, Col, Input, Button, Table, InputGroup, InputGroupAddon, InputGroupText, ButtonGroup } from 'reactstrap'
+import { Form, FormGroup,  Col,  Button, Table, InputGroup,  ButtonGroup, FormControl } from 'react-bootstrap'
 //import { User } from '../../../models/user'
 import { Redirect } from 'react-router'
 import { Dosage } from '../../models/Dosage'
+
 
 
 interface ISubmiteDosageProps
@@ -76,30 +77,39 @@ render() {
 
 
         <Form onSubmit={this.submitNewDosage}>
-                          <FormGroup row>
-                              <Label for="days" sm={3}>How many days</Label>
-                              <Col sm={6}>
-                                  <Input onChange={this.updateDays} value={this.state.days} 
-                                  type="number" name="days" id="days" placeholder="Please Enter How many days?" required />
-                              </Col>
-                              </FormGroup>
-                              <br/>
-                              <FormGroup row>
-                              <Label for="dosageDay" sm={3}>How many Dosage per day</Label>
-                              <Col sm={6}>
-                                  <Input onChange={this.updateDays} value={this.state.days} 
-                                  type="number" name="dosageDay" id="dosageDay" placeholder="Please Enter How many Dosage per day?" required />
-                              </Col>
-                              </FormGroup>
-                              <br/>
-                              <FormGroup row>
-                              <Label for="medicationName" sm={3}>medication Name</Label>
-                              <Col sm={6}>
-                                  <Input onChange={this.updateMedicationName} value={this.state.medicationName} 
-                                  type='text' name="medicationName" id="medicationName" placeholder="Please Enter The medication Name" required />
-                              </Col>
-                              </FormGroup>
-                                                           
+        <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="basic-addon1">Days</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl onChange={this.updateDays} defaultValue={this.state.days} type="number"
+      placeholder="Please Enter How many days?"
+      aria-label="How many days"
+      aria-describedby="basic-addon1"
+    />
+         </InputGroup>
+         <br/>
+         <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="basic-addon1">Dosage/Day</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl onChange={this.updateDosageDay} defaultValue={this.state.dosageDay} type="number"
+      placeholder="Please Enter How many Dosage per day?"
+      aria-label="How many Dosage"
+      aria-describedby="basic-addon1"
+    />
+         </InputGroup>
+         <br/>
+         <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="basic-addon1">medication Name</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl onChange={this.updateMedicationName} defaultValue={this.state.medicationName} type="number"
+      placeholder="Please Enter The medication Name"
+      aria-label="medication Name"
+      aria-describedby="medicationName"
+    />
+         </InputGroup>
+                                               
                               <br/>
                           <Button color='info'>Submite</Button>
                       </Form>
